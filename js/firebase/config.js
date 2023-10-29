29fd78a80433c8df4a96743d4ef225dab1a39dcc
The RituRajPS <theriturajps@gmail.com>
2023-10-29 23:34:32 +0530

    Update config.js

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -6,14 +6,14 @@
 
 
 var firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  databaseURL: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx",
-  measurementId: "xxx"
+  apiKey: "AIzaSyAozAtjCTPbpmUM5ZzBgwjirFUP50GqEyI",
+  authDomain: "gitforgeofficial.firebaseapp.com",
+  databaseURL: "gitforgeofficial-default-rtdb.firebaseio.com",
+  projectId: "gitforgeofficial",
+  storageBucket: "gitforgeofficial.appspot.com",
+  messagingSenderId: "1008435831956",
+  appId: "1:1008435831956:web:80eaaaf504de2401984081",
+  measurementId: "G-KHBR3B2H8B"
 };
 
 
